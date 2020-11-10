064e9e844d72a13fc3d8bef2b6cb344ba4903c74
Filippo-Tortelli <filippo.tortelli@gmail.com>
2020-11-10 20:26:46 +0100

    first round of coding

--- sketch.js
+++ sketch.js
@@ -1,12 +1,60 @@
-function preload(){
-  // put preload code here
-}
+//gui params
+var firstName = "the first name";
+var secondName = "the second name";
+
+//gui
+var visible = true;
+var gui;
+
+var rectangle;
 
 function setup() {
   createCanvas(windowWidth,windowHeight)
-  // put setup code here
+
+  gui = createGui();
+  gui.addGlobals('firstName', 'secondName');
+
+  rectangle = new Rect(width/3, height/2);
 }
 
 function draw() {
-  // put drawing code here
+  background("pink");
+
+  rectMode(CENTER);
+  rectangle.display();
+}
+
+function mouseDragged() {
+  fill(100);
+  rectangle.dragged();
+}
+
+function mouseReleased() {
+  rectangle.released();
+}
+
+//classe per il rettangolo
+class Rect {
+  constructor (temp_x, temp_y) {
+    this.x = temp_x;
+    this.y = temp_y;
+    this.color = 255;
+  }
+
+  display() {
+    fill(this.color);
+    rect(this.x, this.y, 100, 50);
+  }
+
+  dragged() {
+    this.x = mouseX;
+    this.y = mouseY;
+    this.color = 150;
+  }
+
+  released() {
+    // this.x = temp_x;
+    // this.y = temp_y;
+    this.color = 255;
+  }
 }
