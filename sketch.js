858dd8d0d0b0ff3d086b2a7340b305705aec8b5c
Filippo-Tortelli <filippo.tortelli@gmail.com>
2020-11-10 21:22:13 +0100

    second round

--- sketch.js
+++ sketch.js
@@ -5,8 +5,11 @@ var secondName = "the second name";
 //gui
 var visible = true;
 var gui;
+var myText;
 
 var rectangle;
+var dragCompleted = false;
+
 
 function setup() {
   createCanvas(windowWidth,windowHeight)
@@ -15,17 +18,33 @@ function setup() {
   gui.addGlobals('firstName', 'secondName');
 
   rectangle = new Rect(width/3, height/2);
+  myText = round(random(0,100));
 }
 
 function draw() {
   background("pink");
+  push();
+  rectMode(CORNER);
+  noStroke();
+  fill("tomato");
+  rect(width/2,0,width/2,height);
+  pop();
 
+  push();
   rectMode(CENTER);
   rectangle.display();
+  pop();
+
+
+  if(dragCompleted == true) {
+
+    rectangle.printResult();
+  }
 }
 
+//funzioni del mouse: dragged e relaesed
+
 function mouseDragged() {
-  fill(100);
   rectangle.dragged();
 }
 
@@ -33,28 +52,54 @@ function mouseReleased() {
   rectangle.released();
 }
 
+//calcolo compatibilità
+
 //classe per il rettangolo
 class Rect {
   constructor (temp_x, temp_y) {
     this.x = temp_x;
     this.y = temp_y;
+    this.height = 50;
     this.color = 255;
   }
 
   display() {
     fill(this.color);
-    rect(this.x, this.y, 100, 50);
+    rect(this.x, this.y, 100, this.height);
+
   }
 
   dragged() {
     this.x = mouseX;
     this.y = mouseY;
-    this.color = 150;
+    this.color = 200;
   }
 
   released() {
-    // this.x = temp_x;
-    // this.y = temp_y;
-    this.color = 255;
+    if(this.x>width/2) {
+      noStroke();
+      this.color = "tomato";
+      dragCompleted = true;
+      this.height = 0;
+    } else {this.color = 255;}
   }
+
+  printResult() {
+    push();
+    fill(255);
+    textSize(30);
+
+    textAlign(CENTER, CENTER);
+    text("The compatibility between " +firstName+ " and " +secondName+ " is:",3*width/4, height/3);
+    pop();
+
+    push();
+    fill(255);
+    textSize(100);
+
+    textAlign(CENTER, CENTER);
+    text(myText + "%",3*width/4, height/2);
+    pop();
+  }
+
 }
